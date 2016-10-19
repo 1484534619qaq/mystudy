4a14f5b18f89e8bc154274b054906e7395612c85
stu <stu@qq.com>
2016-10-19 11:57:17 +0800

    新建新闻界面

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -39,5 +39,21 @@ namespace WebApplication3.Controllers
 
             return View();
         }
+        public ActionResult newList()
+        {
+            string[] data = new string[] {
+                "斗鱼- 每个人的直播平台提供高清、快捷、流畅的视频直播和游戏",
+                "女主播忘关摄像头是故意还是偶然?斗鱼tv战旗tv熊猫TV都曾发生",
+                "斗鱼主播如何盈利?现在的盈利模式改了吗? ",
+                "斗鱼tv下载_斗鱼tv直播客户端_斗鱼tv直播平台官网"
+            };
+
+           
+                ViewBag.data = data;
+                return View();
+            }
+
+         
+        
     }
 }
