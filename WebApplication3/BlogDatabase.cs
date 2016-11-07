9d60033be89c359bd7458eb863198532c8f5644a
stu <stu@qq.com>
2016-11-07 17:51:46 +0800

    1更新配置文件中的数据库数据库连接串,2blogarticle添加验证修饰，添加博客文章页面使用模型验证

--- WebApplication3/BlogDatabase.cs
+++ WebApplication3/BlogDatabase.cs
@@ -1,6 +1,7 @@
 ﻿namespace WebApplication3
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
     using System.Data.Entity;
     using System.Linq;
 
@@ -68,11 +69,15 @@
         /// <summary>
         /// 标题
         /// </summary>
+        [Required]
+        [StringLength(maximumLength: 20, MinimumLength = 5)]
         public string Subject { get; set; }
 
         /// <summary>
         /// 文章内容
         /// </summary>
+        [Required]
+       
 
         public string Body { get; set; }
 
---
+++ WebApplication3/Controllers/model.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication3.Controllers
+{
+    internal class model
+    {
+        public static string Body { get; internal set; }
+        public static string Subject { get; internal set; }
+    }
+}
